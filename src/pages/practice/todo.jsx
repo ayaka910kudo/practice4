780499eed79e68735da92313c293aed63d676823
ayaka910kudo <aya.caro.brbn8@gmail.com>
2023-10-30 11:27:37 +0900

    タスクの削除機能まで追った

--- src/pages/practice/todo.jsx
+++ src/pages/practice/todo.jsx
@@ -17,13 +17,42 @@ const TodoList = () => {
   ];
 
   const [todos, setTodos] = useState(initTodos);
+  const [task, setTask] = useState("");
+  const handleNewTask = (event) => {
+    console.log(event);
+    setTask(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // デフォルトだとeventを受け取るとリロードされるがページ遷移を防ぐ
+    if (task === "") return; //空文字の場合は何もしない
+    setTodos((todos) => [...todos, { task, isCompleted: false }]); //配列の最後に追加
+    setTask("");
+  };
+
+  const handleRemoveTask = (index) => {
+    const newTodos = [...todos]; //新しい配列をnewTodosにコピー
+    newTodos.splice(index, 1);//index番目から1つ削除
+    setTodos(newTodos);//新しい配列をtodosにセット
+  };
+
 
   return (
     <div>
       <h1>ToDo List</h1>
+
+      <form onSubmit={handleSubmit}>
+        Add Task :
+        <input
+          value={task}
+          placeholder="Add New Task"
+          onChange={handleNewTask}
+        />
+        <button type="submit">Add</button>
+      </form>
       <ul>
         {todos.map((todo, index) => (
-          <li key={index}>{todo.task}</li>
+          <li key={index}>{todo.task} <span onClick={() => handleRemoveTask(index)}>X</span></li>
         ))}
       </ul>
     </div>
